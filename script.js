567a005dfe4d5f25f46a6e886f3523fc79017554
B-MNSN <manutsanan0@gmail.com>
2022-05-20 12:10:21 +0700

    table

--- script.js
+++ script.js
@@ -6,27 +6,27 @@ console.log(student.name)
 
 // document.getElementById('output').innerText = student;
 
-function addRow(container, key, value) {
-    let row = document.createElement('div')
-    row.classList.add('row')
-    let columName = document.createElement('div')
-    columName.classList.add('col-1')
-    columName.classList.add('offset-1')
-    columName.innerHTML = 'key';
-    let columValue = document.createElement('div')
-    columValue.classList.add('col')
-    columValue.innerHTML = value;
-    row.appendChild(columName)
-    row.appendChild(columValue)
-    output.appendChild(row)
-}
+// function addRow(container, key, value) {
+//     let row = document.createElement('div')
+//     row.classList.add('row')
+//     let columName = document.createElement('div')
+//     columName.classList.add('col-1')
+//     columName.classList.add('offset-1')
+//     columName.innerHTML = key;
+//     let columValue = document.createElement('div')
+//     columValue.classList.add('col')
+//     columValue.innerHTML = value;
+//     row.appendChild(columName)
+//     row.appendChild(columValue)
+//     output.appendChild(row)
+// }
 
-function addStudentData(student) {
-    const output = document.getElementById('output')
-    addRow(output, 'ชื่อ', student.name)
-    addRow(output, 'username', student.username)
-    addRow(output, 'เพศ', student.gender)
-}
+// function addStudentData(student) {
+//     const output = document.getElementById('output')
+//     addRow(output, 'ชื่อ', student.name)
+//     addRow(output, 'username', student.username)
+//     addRow(output, 'เพศ', student.gender)
+// }
 var secondStudent = {}
 secondStudent.name = 'น่ารัก'
 secondStudent.username = 'x@y.com'
@@ -43,3 +43,25 @@ var students = [
         gender: 'ชาย'
     }
 ]
+
+function addStudentToTable(index, student) {
+    const tableBody = document.getElementById('tableBody')
+    let row = document.createElement('tr')
+    let cell = document.createElement('th')
+    cell.setAttribute('scope', 'row')
+    cell.innerHTML = index
+    row.appendChild(cell)
+    cell = document.createElement('td')
+    cell.innerHTML = student.name
+    row.appendChild(cell)
+    cell = document.createElement('td')
+    cell.innerHTML = student.username
+    row.appendChild(cell)
+    cell = document.createElement('td')
+    cell.innerHTML = student.gender
+    row.appendChild(cell)
+    tableBody.appendChild(row)
+}
+window.addEventListener('load', function() {
+    addStudentToTable(1, student)
+})
